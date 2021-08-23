1c0514f74b1f1fda9c89ba323c2a33a9682fb2e9
remu <986563206@qq.com>
2021-08-23 16:57:18 +0800

    add io-stream

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # 常用工具类
+
+> ## [io](./src/main/java/top/tocome/io)
+
+- 封装常用一次性流操作
+- 一次性文件读写
+- 一次性网络请求
+- 目录的复制与文件遍历
+
+> ## [control](./src/main/java/top/tocome/control)
+
+- ### [command](./src/main/java/top/tocome/control/command)
+    - 命令行程序
+
+- ### [permission](./src/main/java/top/tocome/control/permission)
+    - 权限管理
---
+++ src/main/java/top/tocome/io/Http.java
@@ -0,0 +1,72 @@
+package top.tocome.io;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Http {
+    /**
+     * 浏览器代理
+     */
+    public static String agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36";
+
+    /**
+     * 获取网络字符串
+     *
+     * @param url http链接
+     * @return 字符串
+     */
+    public static String get(String url) {
+        try {
+            return Stream.readString(getConnection(url).getInputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * post方法
+     *
+     * @param connection HttpURLConnection
+     * @param message    post传递消息
+     * @return post返回消息
+     */
+    public static byte[] post(HttpURLConnection connection, String message) {
+        try {
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            Stream.write(connection.getOutputStream(), message.getBytes());
+            return Stream.readBytes(connection.getInputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new byte[0];
+    }
+
+    /**
+     * 下载网络文件
+     *
+     * @param url      http链接
+     * @param filepath 保存文件路径
+     * @return 执行结果
+     */
+    public static boolean download(String url, String filepath) {
+        try {
+            return Stream.copy(getConnection(url).getInputStream(), new FileOutputStream(filepath));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 获取附带默认{@link #agent User-Agent}的HttpURLConnection
+     */
+    public static HttpURLConnection getConnection(String url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.setRequestProperty("User-Agent", agent);
+        return connection;
+    }
+}
